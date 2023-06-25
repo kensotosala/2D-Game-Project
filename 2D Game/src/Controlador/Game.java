942821ba15f89f97196db17e368561bd65ab2d28
kendevtech <kendallsalazarsoto@gmail.com>
2023-06-24 19:04:17 -0600

    24/06/2023

--- 2D Game/src/Controlador/Game.java
+++ 2D Game/src/Controlador/Game.java
@@ -3,15 +3,31 @@ package Controlador;
 import Vista.GamePanel;
 import Vista.GameWindow;
 
-public class Game {
+public class Game implements Runnable {
     // Atributtes
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
 
     // Constructor
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+    }
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        while (true) {
+            now = System.nanoTime();
+            if (System.nanoTime() - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+            }
+        }
     }
 }
--- 2D Game/src/Vista/GamePanel.java
+++ 2D Game/src/Vista/GamePanel.java
@@ -1,6 +1,8 @@
 package Vista;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -11,11 +13,19 @@ import Modelo.MouseInputs;
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
-    private int xDelta = 100;
-    private int yDelta = 100;
+    private float xDelta = 100;
+    private float yDelta = 100;
+    private float xDir = 0.003f;
+    private float yDir = 0.003f;
+    private int frames = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(150, 20, 90);
+    private Random random;
 
     // Constructor
     public GamePanel() {
+
+        random = new Random();
 
         // Initialize the class
         mouseInputs = new MouseInputs(this);
@@ -30,24 +40,59 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+
     }
 
     // Change the rectangle position
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(xDelta, yDelta, 200, 50);
+        updateRectangle();
+        g.setColor(color);
+        g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
+
+        repaint();
     }
+
+    private void updateRectangle() {
+        xDelta += xDir;
+        if (xDelta > 400 || xDelta < 0) {
+            xDir *= -1;
+            color = getRndColor();
+        }
+
+        yDelta += yDir;
+        if (yDelta > 400 || yDelta < 0) {
+            yDir *= -1;
+            color = getRndColor();
+        }
+
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int b = random.nextInt(255);
+        int g = random.nextInt(255);
+
+        return new Color(r, g, b);
+    }
+
 }
