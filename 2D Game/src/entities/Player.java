a1188a050dac6c0be3317cd1fabd61d9cf377de5
kendevtech <kendallsalazarsoto@gmail.com>
2023-07-23 08:20:46 -0600

    Bigger Levels

--- 2D Game/src/entities/Player.java
+++ 2D Game/src/entities/Player.java
@@ -45,8 +45,9 @@ public class Player extends Entity {
 		setAnimation();
 	}
 
-	public void render(Graphics g) {
-		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset),
+	public void render(Graphics g, int lvlOffset) {
+		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset) - lvlOffset,
+				(int) (hitbox.y - yDrawOffset),
 				width, height, null);
 		// drawHitbox(g);
 	}
@@ -97,8 +98,13 @@ public class Player extends Entity {
 
 		if (jump)
 			jump();
-		if (!left && !right && !inAir)
-			return;
+
+		// if (!left && !right && !inAir)
+		// return;
+
+		if (!inAir)
+			if ((!left && !right) || (right && left))
+				return;
 
 		float xSpeed = 0;
 
--- 2D Game/src/gamestates/Menu.java
+++ 2D Game/src/gamestates/Menu.java
@@ -13,13 +13,14 @@ public class Menu extends State implements Statemethods {
 
 	private MenuButton[] buttons = new MenuButton[3];
 	private BufferedImage backgroundImg;
+	private BufferedImage backgroundImageMain;
 	private int menuX, menuY, menuWidth, menuHeight;
 
 	public Menu(Game game) {
 		super(game);
 		loadButtons();
 		loadBackground();
-
+		backgroundImageMain = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND_IMG);
 	}
 
 	private void loadBackground() {
@@ -46,6 +47,7 @@ public class Menu extends State implements Statemethods {
 	@Override
 	public void draw(Graphics g) {
 
+		g.drawImage(backgroundImageMain, 0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT, null);
 		g.drawImage(backgroundImg, menuX, menuY, menuWidth, menuHeight, null);
 
 		for (MenuButton mb : buttons)
